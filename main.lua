746aae2f20b86b1b7965e0c0eb37347ad558455a
Lord Ubuntu <jacobus.b.business@gmail.com>
2018-12-19 21:04:26 -0800

    modified main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,15 @@
--- Load some default values for our rectangle.
+-- Filler text for the moment, saw it on a web page somewhere
+
+
 function love.load()
     x, y, w, h = 20, 20, 60, 20
 end
- 
--- Increase the size of the rectangle every frame.
+
 function love.update(dt)
     x = x + 1
     y = y * 1.005
 end
- 
--- Draw a coloured rectangle.
+
 function love.draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.rectangle("fill", x, y, w, h)
