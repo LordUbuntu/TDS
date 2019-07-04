959106e2848983914922a9bc12a937f5f5f19653
Jacobus Burger <jacobus.b.business@gmail.com>
2019-07-03 23:00:25 -0700

    experimenting with mouse position

--- main.lua
+++ main.lua
@@ -1,14 +1,15 @@
 -- LOAD --
 function love.load()
-    --
+	--
 end
 
 -- DRAW --
 function love.draw()
-    --
+	love.graphics.print("x: "..mouse_x..", y: "..mouse_y, 0, 0)
+	love.graphics.print("tan: "..math.tan(mouse_y/mouse_x), 100, 0)
 end
 
 -- UPDATE --
 function love.update(dt)
-    --
+	mouse_x, mouse_y = love.mouse.getPosition()
 end
