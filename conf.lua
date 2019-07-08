68fd160cb3b7e9c355dc8b3077c8bed4cab461c6
Jacobus Burger <jacobus.b.business@gmail.com>
2019-07-08 09:59:25 -0700

    added some simple configurations to conf.lua

--- conf.lua
+++ conf.lua
@@ -1,3 +1,17 @@
 function love.conf(t)
-	-- TODO add configuration
+	t.identity = "Top Down Shooter"
+	t.window.title = "TDS"
+	t.window.width = 800
+	t.window.height = 600
+	t.window.resizable = false -- true
+	--t.window.minwidth = 300
+	--t.window.minheight = 200
+	--t.window.fullscreen = true 
+	--t.window.fullscreentype = "desktop"
+
+	-- Disabled Modules
+	t.modules.touch = false
+	--t.modules.joystick = false
+
+	io.stdout:setvbuf("no")
 end
