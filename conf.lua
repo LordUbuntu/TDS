0a5e8f41c550e7e9cb6d485cff5966f78dae203c
Jacobus Burger <therealjacoburger@gmail.com>
2020-03-04 08:14:59 -0800

    added shotgun attack, added .gitignore file

--- conf.lua
+++ conf.lua
@@ -4,14 +4,12 @@ function love.conf(t)
 	t.window.width = 800
 	t.window.height = 600
 	t.window.resizable = false -- true
-	--t.window.minwidth = 300
-	--t.window.minheight = 200
 	--t.window.fullscreen = true 
 	--t.window.fullscreentype = "desktop"
 
 	-- Disabled Modules
 	t.modules.touch = false
-	--t.modules.joystick = false
+	t.modules.joystick = false
 
 	io.stdout:setvbuf("no")
 end
--- main.lua
+++ main.lua
@@ -1,72 +1,25 @@
--- IDEA: instead of creating bullets, draw a shiny line for a moment and then do a quick calculation to determine if an enemy was in the line of fire
--- TODO if mouse is held down go full auto with cooldown between shots
--- TODO boundry collision for window
--- improve movement ??
--- TODO fix line to centre of character
--- TODO find a way to abstract player movement or switch between controll modes (keyboard+mouse VS joystick)
--- TODO collision detection (remove bullets from table on collision)
-
-
--- HANDLERS --
-
--- handle movement events?
--- handle shoot event?
--- etc?
-
-
-
-
-
--- ??? --
-function make_bullet()
-	bullet = { x = player.x-5, y = player.y-2.5, speed = 1500 }
-	bullet.width = 10
-	bullet.height = 5
-	return bullet
-end
-
-
-
-
+-- TODO improve offset of bullets and set them in radius around player
+-- TODO ^^^ with line for player shoot too
+-- TODO make specialized bullet types
 
 -- LOAD --
 function love.load()
-	love.mouse.setGrabbed(false)
-
 	-- player properties
-	player = { x = 50, y = 50, speed = 100, shoot = function() end }
-	player.width = 20
-	player.height = 50
+    player = { speed = 100, width = 20, height = 50 }
+    player.x = 50
+    player.y = 50
 
-	-- bullet properties
+	-- bullet table
 	bullets = {}
+
+    -- other
+    shotgun_splash = 123
 end
-
-
-
-
-
--- DRAW --
-function love.draw()
-	for i, bullet in ipairs(bullets) do
-		love.graphics.rectangle("line", bullet.x, bullet.y, bullet.width, bullet.height)
-	end
-	love.graphics.rectangle("fill", player.x-10, player.y-25, player.width, player.height)
-
-	mouse_x, mouse_y = love.mouse.getPosition()
-	love.graphics.print("player: "..player.x..", "..player.y, 0, 0)
-	love.graphics.print("mouse: "..mouse_x..", "..mouse_y, 0, 10)
-	love.graphics.line(player.x, player.y, mouse_x, mouse_y)
-end
-
-
-
 
 
 -- UPDATE --
 function love.update(dt)
-
-	-- player movement
+	-- update player movement
 	if love.keyboard.isDown('w') then
 		player.y = player.y - player.speed * dt
 	end
@@ -80,42 +33,69 @@ function love.update(dt)
 		player.x = player.x + player.speed * dt
 	end
 
-	-- move bullets
-	function oob(object)
-		if object.x < 0 or object.x > love.graphics.getWidth() then
-			return true
-		elseif object.y < 0 or object.y > love.graphics.getHeight() then
-			return true
-		end
-		return false
+    -- update bullet position
+	for _, bullet in ipairs(bullets) do
+		bullet.x = bullet.x + bullet.dx * dt
+        bullet.y = bullet.y + bullet.dy * dt
+	end
+    -- bullet interaction
+    -- TODO
+end
+
+
+
+-- DRAW --
+function love.draw()
+	-- bullet
+	for _, bullet in ipairs(bullets) do
+        love.graphics.circle("line", bullet.x, bullet.y, bullet.radius)
 	end
 
-	for i, bullet in ipairs(bullets) do
-		bullet.x = bullet.x + bullet.speed * dt
-		if oob(bullet) then
-			table.remove(bullets, i)
-		end
-	end
+	-- player
+	love.graphics.rectangle("fill", player.x-10, player.y-25, player.width, player.height)
 
+	mouse_x, mouse_y = love.mouse.getPosition()
+	love.graphics.print("player: "..player.x..", "..player.y, 0, 0)
+	love.graphics.print("mouse: "..mouse_x..", "..mouse_y, 0, 15)
+	love.graphics.line(player.x, player.y, mouse_x, mouse_y)
 end
 
 
 
 
 
--- ??? --
 
+
+-- EVENTS --
 function love.mousepressed(x, y, button, istouch)
 	if button == 1 then	-- lmb click
-		-- determine direction
-		-- set speed according to x and y velocity from sin and cos of mouse x,y relative to player
-		-- set bullet to that area just outside player hitbox
-		table.insert(bullets, make_bullet())
-	end
+        love.event.push("shoot", x, y)
+	elseif button == 2 then -- rmb click
+        for i = 1, 5 do
+            love.event.push("shoot", 
+                            x + math.random(shotgun_splash), 
+                            y + math.random(shotgun_splash))
+        end
+    end
 end
 
+-- handle movement events?
 function love.keypressed(key)
 	if key == "escape" then
 		love.event.push("quit")
 	end
+end
+
+-- HANDLERS --
+function love.handlers.shoot(x, y)
+    -- TODO set bullet outside radius from player
+    local bullet = { radius = 5, speed = 500 }
+    bullet.x = player.x + 40 / 2
+    bullet.y = player.y + 40 / 2
+    -- determine Δy and Δx from atan
+    local angle = math.atan2((y - bullet.y), (x - bullet.x))
+    bullet.dx = bullet.speed * math.cos(angle)
+    bullet.dy = bullet.speed * math.sin(angle)
+    -- record bullet 
+    table.insert(bullets, bullet)
 end
