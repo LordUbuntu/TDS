9255269c724cad727626b70013ef277e884826e3
Jacobus Burger <therealjacoburger@gmail.com>
2020-05-10 18:56:16 -0700

    added collision detection and dummy enemies

--- main.lua
+++ main.lua
@@ -1,7 +1,16 @@
 -- TODO
--- add enemies
--- add collision detection for bullets and enemies, and for enemies and player (none for player and bullet)
+-- add health for player and enemies
+-- add damage for bullets
 -- make enemies move towards player
+-- make binaries and document build instructions
+-- publish, merge branches, and archive repo
+
+
+-- XXX done
+-- collision detection
+-- enemies added
+
+
 
 -- LOAD --
 function love.load()
@@ -9,15 +18,28 @@ function love.load()
     pl = {  max_v = 10, dv = 30, kf = 0.5,
             dx = 0, dy = 0,
             x = 100, y = 100,
-            w = 5, h = 5 }
+            r = 10 }
     pl.sprite = love.graphics.newImage("assets/sprites/player.png")
     -- player score record
     score = 0
+    if not love.filesystem.getInfo("score.txt") then
+        love.filesystem.write("score.txt", score)
+    end
     high_score = tonumber(love.filesystem.read("score.txt"), 10)
 
 
 	-- bullet table
 	bullets = {}
+
+
+    -- enemies table
+    enemies = {}
+    enemy = {
+        x = 200,
+        y = 50,
+        r = 10,
+    }
+    table.insert(enemies, enemy)
 end
 
 -- UPDATE --
@@ -40,21 +62,21 @@ function love.update(dt)
 
     -- stop player at window edge
     width, height = love.graphics.getDimensions()
-    if pl.x - pl.w < 0 then
+    if pl.x - pl.r * 2 < 0 + 5 then
         pl.dx = 0
-        pl.x = pl.x + pl.w
+        pl.x = pl.x + pl.r * 2
     end
-    if pl.x + pl.w > width then
+    if pl.x + pl.r * 2 > width - 5 then
         pl.dx = 0
-        pl.x = pl.x - pl.w
+        pl.x = pl.x - pl.r * 2
     end
-    if pl.y - pl.h < 0 then
+    if pl.y - pl.r * 2 < 0 + 5 then
         pl.dy = 0
-        pl.y = pl.y + pl.h
+        pl.y = pl.y + pl.r * 2
     end
-    if pl.y + pl.h > height then
+    if pl.y + pl.r * 2 > height - 5 then
         pl.dy = 0
-        pl.y = pl.y - pl.h
+        pl.y = pl.y - pl.r * 2
     end
 
     -- apply position
@@ -89,36 +111,65 @@ function love.update(dt)
 
 
 
-    ----- HANDLE BULLET MOVEMENT -----
 	for _, bullet in ipairs(bullets) do
+        ----- HANDLE BULLET MOVEMENT -----
 		bullet.x = bullet.x + bullet.dx * dt
         bullet.y = bullet.y + bullet.dy * dt
 	end
+
+
+
+    ----- HANDLE COLLISION DETECTION -----
+    for _, bullet in ipairs(bullets) do
+        -- check if bullet collides with enemy
+        if collision(bullet, enemy) then
+            print("collision: bullet, enemy")
+        end
+    end
+    for _, enemy in ipairs(enemies) do
+        -- check if enemy collied with player
+        if collision(enemy, pl) then
+            print("collision: enemy, player")
+        end
+    end
 end
 
 -- DRAW --
 function love.draw()
-	-- bullet
+	-- draw bullets
 	for _, bullet in ipairs(bullets) do
-        love.graphics.circle("fill", bullet.x, bullet.y, bullet.radius)
+        love.graphics.circle("fill", bullet.x, bullet.y, bullet.r)
 	end
 
+    -- draw enemies
+    for _, enemy in ipairs(enemies) do
+        love.graphics.circle("line", enemy.x, enemy.y, enemy.r)
+    end
+
+    -- draw player
 	mouse_x, mouse_y = love.mouse.getPosition()
-
     local angle = math.atan2(mouse_y - pl.y, mouse_x - pl.x) + math.pi / 2
-    love.graphics.draw(pl.sprite, pl.x, pl.y, angle, 1, 1, pl.w, pl.h)
+    love.graphics.draw(pl.sprite, pl.x, pl.y, angle, 1, 1, pl.r / 2, pl.r / 2)
 
+    -- print hud
     love.graphics.print(score, 0, 0)
     love.graphics.print(high_score, 0, 15)
 end
 
-
 -- HELPERS --
 function collision(entity1, entity2)
-    -- check if entity1 collided with entity 2 vertically
-        -- and horizontally
-            -- if so return true, else false
+    -- assumes all entities are spheres and checks if they overlap
+    local dx = entity1.x - entity2.x
+    local dy = entity1.y - entity2.y
+    local distance = math.sqrt(dx^2 + dy^2)
+    if distance < entity1.r + entity2.r then
+        return true
+    else
+        return false
+    end
 end
+
+
 
 
 
@@ -147,7 +198,7 @@ end
 function love.handlers.shoot(x, y)
     -- create bullet table
     local bullet = {
-        radius = 2,
+        r = 2,
         x = 0, y = 0,
         dv = 1000, dx = 0, dy = 0,
     }
